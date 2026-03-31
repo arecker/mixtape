147a93005141ec8e45756ed697bcd6f8ed65050c
Alex Recker <alex@reckerfamily.com>
2026-03-30 21:58:33 -0500

    Just use regular case in config.

--- mixtape/convert.py
+++ mixtape/convert.py
@@ -18,9 +18,9 @@ def convert_track(source: str | pathlib.Path, destination: str | pathlib.Path):
 
 def tag_track(target: str | pathlib.Path, track: Track, number: int, album: str, year: int, cover=None):
     tags = id3.ID3()
-    tags.add(id3.TIT2(encoding=3, text=track.title.title()))
-    tags.add(id3.TPE1(encoding=3, text=track.artist.title()))
-    tags.add(id3.TALB(encoding=3, text=album.title()))
+    tags.add(id3.TIT2(encoding=3, text=track.title))
+    tags.add(id3.TPE1(encoding=3, text=track.artist))
+    tags.add(id3.TALB(encoding=3, text=album))
     tags.add(id3.TRCK(encoding=3, text=str(number)))
     tags.add(id3.TYER(encoding=3, text=str(year)))
 
